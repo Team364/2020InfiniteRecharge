b05ae2717fca5ce8065ec37542581c54a0d99bb3
Camden Torgesen <59778810+Ctorgy@users.noreply.github.com>
2020-02-08 14:05:10 -0600

    fixed an error

--- src/main/java/com/team364/frc2020/Robot.java
+++ src/main/java/com/team364/frc2020/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  private RobotContainer m_robotContainer;.
+  private RobotContainer m_robotContainer;
 
   /**
    * This function is run when the robot is first started up and should be used
