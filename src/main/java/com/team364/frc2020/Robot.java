f10670431bbaf4e8c631223d607d12708c665536
Camden Torgesen <59778810+Ctorgy@users.noreply.github.com>
2020-02-08 14:00:22 -0600

    implemented code for color sensor

--- src/main/java/com/team364/frc2020/Robot.java
+++ src/main/java/com/team364/frc2020/Robot.java
@@ -11,6 +11,10 @@ package com.team364.frc2020;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.ColorSensorV3;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,9 +24,11 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
+  private I2C.Port i2cPort = I2C.Port.kOnboard;
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private RobotContainer m_robotContainer;.
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -48,6 +54,19 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    Color detectedColor = m_colorSensor.getColor();
+
+    double IR = m_colorSensor.getIR();
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", IR);
+
+    int proximity = m_colorSensor.getProximity();
+
+    SmartDashboard.putNumber("Proximity", proximity);
   }
 
   /**
